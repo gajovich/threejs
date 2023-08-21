ef245e7a305defbd02100d3ba66d1fb0977a7a5e
gajovich <gajovich79@gmail.com>
2023-08-21 15:52:47 +0300

    new

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,35 @@
+import { useSnapshot } from 'valtio';
+import { getContrastingColor } from '../config/helpers';
+import state from '../store';
+
+/* eslint-disable react/prop-types */
+const CustomButton = ({ type, title, handleClick, customStyles }) => {
+  const snap = useSnapshot(state);
+
+  const generateStyle = (type) => {
+    if (type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: getContrastingColor(snap.color),
+      };
+    } else if (type === 'outline') {
+      return {
+        borderWidth: '1px',
+        borderColor: snap.color,
+        color: snap.color,
+      };
+    }
+  };
+
+  return (
+    <button
+      type={type}
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}>
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
